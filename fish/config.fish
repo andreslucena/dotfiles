5e9e9e789f9a8e6542082e2dc26094bbc8516716
Andrés Pereira de Lucena <andreslucena@gmail.com>
2025-04-10 07:40:07 +0000

    Add conditional prompt in pavement

--- fish/config.fish
+++ fish/config.fish
@@ -64,3 +64,21 @@ direnv hook fish | source
 if test -f /home/apereira/.nix-profile/bin/mise
     ~/.nix-profile/bin/mise activate fish | source
 end
+
+if test (hostname) = pavement
+    function fish_prompt
+        set_color normal
+        # https://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
+        set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+        echo -n (whoami)'@'(hostname)':'
+        set_color $fish_color_cwd
+        echo -n (prompt_pwd)
+        set_color normal
+        echo -n '{'
+        set_color purple
+        echo -n "$git_branch"
+        set_color normal
+        echo -n '}'
+        echo -n ' $ '
+    end
+end
